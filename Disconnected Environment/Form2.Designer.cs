141d24128f1cfe8a8a9f269c3d0afae3bfcd2214
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2023-06-22 16:23:01 +0700

    Add project files.

---
+++ Disconnected Environment/Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Disconnected_Environment
+{
+    partial class FormDataProdi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.nmp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(143, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(519, 292);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Location = new System.Drawing.Point(476, 340);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSave.Location = new System.Drawing.Point(314, 368);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClear.Location = new System.Drawing.Point(395, 368);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.SystemColors.Control;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOpen.Location = new System.Drawing.Point(695, 31);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            // 
+            // nmp
+            // 
+            this.nmp.AutoSize = true;
+            this.nmp.Location = new System.Drawing.Point(229, 343);
+            this.nmp.Name = "nmp";
+            this.nmp.Size = new System.Drawing.Size(79, 16);
+            this.nmp.TabIndex = 1;
+            this.nmp.Text = "Nama Prodi";
+            // 
+            // FormDataProdi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nmp);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormDataProdi";
+            this.Text = "Data Prodi";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label nmp;
+    }
+}
---
+++ Disconnected Environment/Form3.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disconnected_Environment
+{
+    public partial class FormDataMahasiswa : Form
+    {
+        private string stringConnection = "data source = DEN\\DENMUHAMMAD" +
+        "database=activity6;User ID=sa;Password=12345678";
+        private SqlConnection koneksi;
+        private string nim, nama, alamat, jk, prodi;
+        private DateTime tgl;
+        BindingSource customerBindingSource = new BindingSource();
+
+        private void FormDataMahasiswa_Load()
+        {
+            koneksi.Open();
+            SqlDataAdapter dataAdapter1 = new SqlDataAdapter(new SqlCommand("Select m.nim, m.nama_mahasiswa, " +
+            "m.alamat, m.jenis_kel, m.tgl_lahir, p.nama_prodi From dbo.mahasiswa m" +
+            "join dbo.prodi p on m.id_prodi = p.id_prodi", koneksi));
+            DataSet ds = new DataSet();
+            dataAdapter1.Fill(ds);
+
+            this.customerBindingSource.DataSource = ds.Tables[0];
+            this.txtNIM.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "NIM", true));
+            this.txtNama.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "nama_mahasiswa", true));
+            this.txtAlamat.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "alamat", true));
+            this.cbxJenisKelamin.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "jenis_kel", true));
+            this.dtTanggalLahir.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "tgl_lahir", true));
+            this.cbxProdi.DataBindings.Add(
+                new Binding("Text", this.customerBindingSource, "nama_prodi", true));
+            koneksi.Close();
+        }
+
+        private void clearBinding()
+        {
+            this.txtNIM.DataBindings.Clear();
+            this.txtNama.DataBindings.Clear();
+            this.txtAlamat.DataBindings.Clear();
+            this.cbxJenisKelamin.DataBindings.Clear();
+            this.dtTanggalLahir.DataBindings.Clear();
+            this.cbxProdi.DataBindings.Clear();
+        }
+
+        private void refreshform()
+        {
+            txtNIM.Enabled = false;
+            txtNama.Enabled = false;
+            cbxJenisKelamin.Enabled = false;
+            txtAlamat.Enabled = false;
+            dtTanggalLahir.Enabled = false;
+            cbxProdi.Enabled = false;
+            btnAdd.Enabled = true;
+            btnSave.Enabled = false;
+            btnClear.Enabled = false;
+            clearBinding();
+            FormDataMahasiswa_Load();
+        }
+        public FormDataMahasiswa()
+        {
+            InitializeComponent();
+            koneksi = new SqlConnection(stringConnection);
+            this.bnMahasiswa.BindingSource = this.customerBindingSource;
+            refreshform();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            nim = txtNIM.Text;
+            nama = txtNama.Text;
+            jk = cbxJenisKelamin.Text;
+            alamat = txtAlamat.Text;
+            tgl = dtTanggalLahir.Value;
+            prodi = cbxProdi.Text;
+            int hs = 0;
+            koneksi.Open();
+            string strs = "select id_prodi from dbo.prodi where nama_prodi = @dd";
+            SqlCommand cm = new SqlCommand(strs, koneksi);
+            cm.CommandType = CommandType.Text;
+            cm.Parameters.Add(new SqlParameter("@dd", prodi));
+            SqlDataReader dr = cm.ExecuteReader();
+            while (dr.Read())
+            {
+                hs = int.Parse(dr["id_prodi"].ToString());
+            }
+            dr.Close();
+            string str = "insert into dbo.mahasiswa (nim, nama_mahasiswa, jenis_kelamin, alamat, tgl_lahir, id_prodi" +
+            "values(@NIM, @Nm, @Jk, @Al, @Tgll, @Idp";
+            SqlCommand cmd = new SqlCommand(str, koneksi);
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Add(new SqlParameter("NIM", nim));
+            cmd.Parameters.Add(new SqlParameter("Nm", nama));
+            cmd.Parameters.Add(new SqlParameter("Jk", jk));
+            cmd.Parameters.Add(new SqlParameter("Al", alamat));
+            cmd.Parameters.Add(new SqlParameter("Tgll", tgl));
+            cmd.Parameters.Add(new SqlParameter("Idp", hs));
+            cmd.ExecuteNonQuery();
+
+            koneksi.Close();
+
+            MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            refreshform();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshform();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            txtNIM.Text = "";
+            txtNama.Text = "";
+            txtAlamat.Text = "";
+            dtTanggalLahir.Value = DateTime.Today;
+            txtNIM.Enabled = true;
+            txtNama.Enabled = true;
+            cbxJenisKelamin.Enabled = true;
+            txtAlamat.Enabled = true;
+            dtTanggalLahir.Enabled = true;
+            cbxProdi.Enabled = true;
+            Prodicbx();
+            btnSave.Enabled = true;
+            btnClear.Enabled = true;
+            btnAdd.Enabled = false;
+        }
+
+        private void Prodicbx()
+        {
+            koneksi.Open();
+            string str = "Select nama_prodi from dbo.prodi";
+            SqlCommand cmd = new SqlCommand(str, koneksi);
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteReader();
+            koneksi.Close();
+            cbxProdi.DisplayMember = "nama_prodi";
+            cbxProdi.ValueMember = "id_prodi";
+            cbxProdi.DataSource = ds.Tables[0];
+        }
+
+        private void FormDataMahasiswa_FormClosed(Object sender, FormClosedEventArgs e)
+        {
+            FormHalamanUtama fm = new FormHalamanUtama();
+            fm.Show();
+            this.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
